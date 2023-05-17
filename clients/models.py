28a5082e9f866dea2aeaaa486164e09fd20122de
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-05-17 15:11:49 +0200

    Added basic models

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField()
+    phone = models.CharField(max_length=30, blank=True, null=True)
+    mobile = models.CharField(max_length=30, blank=True, null=True)
+    company_name = models.CharField(max_length=250, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    sales_contact = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        limit_choices_to={"team_id": 2},
+    )
--- events/models.py
+++ events/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
+from contracts.models import Contract
 
-# Create your models here.
+
+class Event(models.Model):
+    contract = models.OneToOneField(
+        to=Contract,
+        on_delete=models.CASCADE,
+        limit_choices_to={"status": True},
+        related_name="event",
+    )
+    name = models.CharField(max_length=100, null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    support_contact = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        limit_choices_to={"team_id": 3},
+    )
+    event_status = models.BooleanField(default=False, verbose_name="Completed")
+    attendees = models.PositiveIntegerField()
+    event_date = models.DateTimeField()
+    notes = models.TextField(null=True, blank=True)
