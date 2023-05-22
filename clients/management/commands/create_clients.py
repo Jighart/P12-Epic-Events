0005cce608432b1a1bdfa688d9513ad14619e0d2
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-05-22 19:32:00 +0200

    Added management commands to create data

--- clients/management/commands/create_clients.py
+++ clients/management/commands/create_clients.py
@@ -1,41 +1,49 @@
-import random
+from random import choice
 
 from django.core.management import BaseCommand
 from faker import Faker
 
+from clients.models import Client
 from users.models import User
 
 
 class Command(BaseCommand):
-    help = "Create sample users."
+    help = "Create sample clients data."
 
     def add_arguments(self, parser):
         parser.add_argument(
             "--number",
             "-n",
             dest="number",
-            default=5,
+            default=10,
             type=int,
-            help="Specify the number of users to create.",
+            help="Specify the number of clients to create.",
         )
 
     def handle(self, *args, **options):
         fake = Faker()
         number = options["number"]
         if options["verbosity"] != 0:
-            self.stdout.write(f"Creating {number} user(s)...")
-        self.create_users(fake, number)
+            self.stdout.write(f"Creating {number} client(s)...")
+        self.create_clients(fake, number)
 
     @staticmethod
-    def create_users(fake, number):
+    def create_clients(fake, number):
+        sales = User.objects.filter(team='SALES').values_list("pk", flat=True)
         for _ in range(number):
-            User.objects.create_user(
+            status = fake.pybool()
+            if status:
+                contact = choice(sales)
+            else:
+                contact = None
+
+            Client.objects.create(
                 first_name=fake.first_name(),
                 last_name=fake.last_name(),
-                username=f"{fake.user_name()}{random.randint(1, 99)}",
-                password=fake.password(length=8),
+                company_name=fake.company(),
                 email=fake.ascii_safe_email(),
                 phone=fake.phone_number(),
                 mobile=fake.phone_number(),
-                team=random.choices(['SALES', 'SUPPORT', 'MANAGEMENT'], k=1)[0],
+                status=status,
+                sales_contact_id=contact,
             )
