a0e03b33395a1f6da215372067870d473d0181af
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-05-28 19:00:03 +0200

    Fixed sales team permissions to limit own clients

--- clients/permissions.py
+++ clients/permissions.py
@@ -6,18 +6,20 @@ from clients.models import Client
 
 class ClientPermissions(permissions.BasePermission):
     """
-    Sales/Management team : can CREATE/VIEW/UPDATE clients and DELETE prospects only
+    Sales team : can CREATE new clients / prospects
+                 can VIEW and UPDATE any prospect and their own clients
+                 can DELETE prospects only
     Support team : can VIEW clients they are assigned to
     """
 
     def has_permission(self, request, view):
         if request.user.team == SUPPORT:
             return request.method in permissions.SAFE_METHODS
-        return request.user.team == SALES or request.user.team == MANAGEMENT
+        return request.user.team in {SALES, MANAGEMENT}
 
     def has_object_permission(self, request, view, obj):
         if request.method == "DELETE":
-            return request.user.team == SALES and obj.status is False
+            return request.user.team in {SALES, MANAGEMENT} and obj.status is False
         elif request.user.team == SUPPORT and request.method in permissions.SAFE_METHODS:
             return obj in Client.objects.filter(contract__event__support_contact=request.user)
-        return request.user.team == SALES or request.user.team == MANAGEMENT
+        return request.user == obj.sales_contact or obj.status is False or request.user.team == MANAGEMENT
