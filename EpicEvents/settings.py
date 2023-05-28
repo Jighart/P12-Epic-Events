3b942e0abf8c69184e90a9614d4ef2cb375785b1
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-05-28 17:58:01 +0200

    Minor correction

--- EpicEvents/settings.py
+++ EpicEvents/settings.py
@@ -102,7 +102,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication'
     ],
     "DATE_FORMAT": "%d-%m-%Y",
